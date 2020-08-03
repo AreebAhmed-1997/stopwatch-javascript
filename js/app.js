a9f15452a1d87560740156e6f18a9216f130a16b
areeb1997ahmed <areeb1997ahmed@gmail.com>
2020-08-03 18:53:07 +0500

    stopwatch

---
+++ js/app.js
@@ -0,0 +1,26 @@
+tim_msec = 0; 
+tim_sec = 0; 
+tim_min =0;
+tim_hour= 0;
+var nummilsec  = document.getElementById("tim_msec");
+var numsec  = document.getElementById("tim_sec");
+var nummin  = document.getElementById("tim_min");
+var numhour  = document.getElementById("tim_hour");
+var interval;
+ 
+function timer (){
+    tim_msec++
+    nummilsec.innerHTML = tim_msec;
+    if (tim_msec>=99){
+        tim_sec ++
+        numsec.innerHTML = tim_sec
+        tim_msec = 0
+    }
+    else if ( tim_sec>=59){
+        tim_min++
+        nummin.innerHTML = tim_min;
+        tim_sec=0
+    }
+}
+ interval = setInterval( timer , 10)
+ 
